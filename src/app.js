3cd8fd3675c4e4bf3281bbaf6e846d3fe51831e4
Gabriel D'Andréa <webgabrielaraujo@gmail.com>
2023-12-05 10:09:34 -0500

    implementando middlewares de erro

--- src/app.js
+++ src/app.js
@@ -67,4 +67,20 @@ app.delete('/teams/:id', (req, res) => {
   res.sendStatus(204);
 });
 
+// GET - teams.json | (Middlewares de erros)
+
+app.get('/teamss', async (req, res, next) => {
+  try {
+    const data = await fs.readFile(path.resolve(__dirname, './teams.json'));
+    const teams = JSON.parse(data);
+    return res.status(200).json({ teams })
+  } catch (error) {
+    return next(error);
+  }
+});
+
+app.use((error, _req, res, _next) => {
+  return res.status(500).json({ error: error.message });
+});
+
 module.exports = app;
